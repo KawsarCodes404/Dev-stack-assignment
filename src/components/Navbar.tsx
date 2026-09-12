34a17c7a1e0028432110bb424d2858f692fe367c
Ridwanulhaquekawsar <kawsarhaque430@gmail.com>
2026-09-13 04:16:48 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return (
+        <nav>
+            {/* left */}
+            <div>
+
+            </div>
+
+            {/* middle */}
+            <div>
+
+            </div>
+
+            {/* right */}
+            <div>
+                
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
