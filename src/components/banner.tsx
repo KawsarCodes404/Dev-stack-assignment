6158f51978adf78e510b20622bebe2fd341c85c4
Ridwanulhaquekawsar <kawsarhaque430@gmail.com>
2026-09-13 07:49:26 +0600

    my third commit while i am trying change the font weight of text

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -7,20 +7,20 @@ const banner = () => {
                 {/* left */}
                 <div>
                     {/* left-heading */}
-                    <h1 className="">
+                    <h1 className="font-bold text-7xl">
                         Build Your Ideal
 
                         <br />
 
-                        <span className="">
+                        <span className="bg-gradient-to-r from-orange-500 to-pink-600 bg-clip-text text-transparent">
                             Development Stack
                         </span>
                     </h1>
 
                     {/* Description start */}
-                    <p className="">
-                        Explore frontend, backend, database, and tooling options,
-                        compare them side by side, and put together the stack that fits your
+                    <p className="mt-[30px] text-[21px] font-[100]">
+                        Explore frontend, backend, database, and tooling options,<br />
+                        compare them side by side, and put together the stack that fits your <br />
                         next project.
                     </p>
 
